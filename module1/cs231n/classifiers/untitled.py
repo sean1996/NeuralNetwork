f66edb9010bf368e0761e5b8e8cee2d2ba037a1d
Xinghan Wang <xinghanw@usc.edu>
2017-03-23 21:20:59 -0700

    Rename

---
+++ module1/cs231n/classifiers/untitled.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import tensorflow.examples.tutorials.mnist.input_data as input_data
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+print ("Packages imported")
+
+
+
